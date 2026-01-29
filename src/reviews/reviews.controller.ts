7a9ce993cdaacf3511a7dec3c96e57a970bbc409
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-29 18:09:42 -0500

    Create route and methods for obtaining due reviews

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ReviewsService } from './reviews.service';
+import { ReviewsDueDto } from 'src/dto/reviews-due.dto';
 
 @Controller('reviews')
-export class ReviewsController {}
+export class ReviewsController {
+  constructor(private readonly reviewsService: ReviewsService) {}
+
+  @Get('due')
+  async getDueReviews(): Promise<ReviewsDueDto[]> {
+    return this.reviewsService.getReviewsDue();
+  }
+}
--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ReviewsDueDto } from 'src/dto/reviews-due.dto';
 
 @Injectable()
-export class ReviewsService {}
+export class ReviewsService {
+  constructor(private prisma: PrismaService) {}
+
+  async getReviewsDue(): Promise<ReviewsDueDto[]> {
+    return this.prisma.currentReview.findMany({
+      where: {
+        nextReviewAt: {
+          lte: new Date(),
+        },
+      },
+      orderBy: {
+        nextReviewAt: 'asc',
+      },
+      select: {
+        userId: true,
+        problemNumber: true,
+        problem: {
+          select: {
+            title: true,
+          },
+        },
+      },
+    });
+  }
+
+  // async getReviewDueDate(params: { problemNumber: number }) {
+  //   const { problemNumber } = params;
+
+  //   return this.prisma.currentReview({})
+  // }
+}
