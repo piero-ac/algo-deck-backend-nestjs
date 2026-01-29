15505cae3fcb1621514e3211196220d106085bfe
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-29 11:30:54 -0500

    Import Prisma Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ProblemsService } from './problems/problems.service';
+import { ProblemsController } from './problems/problems.controller';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProblemsController],
+  providers: [AppService, ProblemsService, PrismaService],
 })
 export class AppModule {}
