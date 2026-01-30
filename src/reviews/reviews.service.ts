15531607aa88f155739782e56cd68b3fcae6c6fc
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-29 19:43:21 -0500

    Begin working on submitReview logic

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ReviewsDueDto } from 'src/dto/reviews-due.dto';
+import { Rating } from 'src/generated/prisma/enums';
 
 @Injectable()
 export class ReviewsService {
@@ -44,7 +45,32 @@ export class ReviewsService {
     });
   }
 
-  // async submitReview(params: { userId: string; problemNumber: string; rating: string}) {
+  async submitReview(params: {
+    userId: number;
+    problemNumber: number;
+    rating: number;
+  }) {
+    const { userId, problemNumber, rating } = params;
+    const result = await this.getReviewDueDate({ userId, problemNumber });
 
-  // }
+    if (!result) {
+      throw new Error('NO_REVIEW_HISTORY');
+    }
+
+    const now = new Date();
+    if (result.nextReviewAt > now) {
+      throw new Error('REVIEW_NOT_DUE');
+    }
+
+    /**
+     * Start transcation for submission
+     * 1. Fetch the row from current_reviews
+     * 2. Create a card from Row (if exists) (fsrs.rowToCard) or new Card with (fsrs.createNewCard) now
+     * 3. Run FSRS
+     * 4. Map rating number to Rating enum (check for invalid rating)
+     * 5. Upsert the card - current_reviews (cardToRowForCurrentReviews)
+     * 6. Upload review log - review_history (cardToRowForReviewHistory)
+     * 6. Return { nextReviewAt }
+     */
+  }
 }
