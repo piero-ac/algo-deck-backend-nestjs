5cc4d43d1bd0453ef8c9cebed9e3eb9cef528eeb
Piero Coronado Flores <pacoronado01@gmail.com>
2026-02-01 16:26:00 -0500

    Edit Schema, Repopulate Problems DB

--- src/scripts/problems.migrate.ts
+++ src/scripts/problems.migrate.ts
@@ -1,17 +1,21 @@
-// import-csv.ts
+// src/scripts/import-csv.ts
 import 'dotenv/config';
 import fs from 'fs';
 import path from 'path';
 import csv from 'csv-parser';
 import { PrismaClient, Prisma } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+if (!process.env.DATABASE_URL) throw new Error('.env DATABASE_URL not found!');
+
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
   const results: Prisma.ProblemCreateManyInput[] = [];
-
-  const csvFilePath = path.resolve('scripts/problems.csv');
+  const csvFilePath = path.join(__dirname, 'problems.csv'); // CSV in same folder as script
 
   await new Promise<void>((resolve, reject) => {
     fs.createReadStream(csvFilePath)
@@ -27,7 +31,6 @@ async function main() {
           similarProblemNumbers: row.similar_problem_number ?? '',
           similarProblemTexts: row.similar_problem_text ?? '',
         };
-
         results.push(record);
       })
       .on('end', () => resolve())
@@ -37,12 +40,10 @@ async function main() {
   const batchSize = 1000;
   for (let i = 0; i < results.length; i += batchSize) {
     const batch = results.slice(i, i + batchSize);
-
     const res = await prisma.problem.createMany({
       data: batch,
       skipDuplicates: true,
     });
-
     console.log(`Inserted batch: ${res.count} problems`);
   }
 
