37e62a664388c65f7d82b1873f786cbdef5c0b33
Piero Coronado Flores <pacoronado01@gmail.com>
2026-02-05 13:42:40 -0500

    Add frontend secret middleware

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { frontendSecretOnly } from './middleware/frontendRestriction.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  app.use(frontendSecretOnly);
 
   app.useGlobalPipes(
     new ValidationPipe({
