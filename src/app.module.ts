bbc8c7fbffe0a0c43f06262248268aced2aff902
Piero Coronado Flores <pacoronado01@gmail.com>
2026-02-05 13:45:03 -0500

    Import throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,20 @@ import { ReviewsService } from './reviews/reviews.service';
 import { FsrsService } from './fsrs/fsrs.service';
 import { ReviewStatsService } from './reviews/review-stats.service';
 import { UsersService } from './users/users.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60,
+          limit: 30,
+        },
+      ],
+    }),
+  ],
   controllers: [AppController, ProblemsController, ReviewsController],
   providers: [
     AppService,
